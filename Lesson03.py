0aaa81e07eb687fb085ff3f1ce8f23c3e617ea23
hieubdn <hieubdn@gmail.com>
2024-03-03 13:03:42 +0700

    u

---
+++ Lesson03.py
@@ -0,0 +1,9 @@
+# dola = input("Nhập số $ cần chuyển: ")
+# vnd = int(dola) * 23470
+# print(f"$ {dola} chuyển sang được {vnd} VNĐ")
+
+n = int(input("Nhập số n: "))
+if n % 2 == 0: #Nếu n chia 2 có số dư bằng 0
+    print(f"Số {n} là số chẵn")
+else:
+    print(f"Số {n} là số lẻ")
